fdf7cced8389e596008648a28463ea0460336723
e-bold <enkhmunkh@att.net>
2024-05-29 10:05:18 -0500

    Initial commit of ASPNET class example

---
+++ HelloASPDotNet/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNet;
+
+
+[Route("/helloworld")]
+public class HelloController : Controller 
+{
+    // GET: /helloworld/
+    [HttpGet]
+   public IActionResult Index() 
+   {
+      string html = "<form method='post' action='/helloworld/welcome'>" +
+      "<input type = 'text' name='name' />" +
+      "<input type = 'submit' value='Greet me!' />" +
+      "</form>";
+      return Content(html, "text/html");
+   }
+
+   //Get: /helloworld/welcome
+    // POST: /helloworld/welcome
+    [HttpGet("welcome/{name?}")]
+    [HttpPost("welcome")]
+    public IActionResult Welcome(string name = "World")
+    {
+        return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+    }
+ 
+
+}
