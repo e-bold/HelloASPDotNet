4305691d325117613be753f2c7bf9a3104611ba7
e-bold <enkhmunkh@att.net>
2024-06-02 15:16:12 -0500

    Added some changes

--- HelloASPDotNet/Controllers/HelloController.cs
+++ HelloASPDotNet/Controllers/HelloController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.Design;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HelloASPDotNet;
 
@@ -6,25 +7,64 @@ namespace HelloASPDotNet;
 [Route("/helloworld")]
 public class HelloController : Controller 
 {
-    // GET: /helloworld/
-    [HttpGet]
+// GET: /helloworld
+[HttpGet]
    public IActionResult Index() 
    {
-      string html = "<form method='post' action='/helloworld/welcome'>" +
+      string html = "<form method='post' action='/helloworld'>" +
       "<input type = 'text' name='name' />" +
+      "<select name = 'language'><option value ='english' selected>English</option>" +
+      "<option value = 'spanish'>Spanish</option>" +
+      "<option value = 'bosnian'>Bosnian</option>" +
+      "<option value = 'mongolian'>Mongolian</option>" +
+      "<option value = 'french'>French</option>"+
+      "</select>"+
       "<input type = 'submit' value='Greet me!' />" +
       "</form>";
       return Content(html, "text/html");
    }
+   //POST: /helloworld
+   [HttpPost]
+    public IActionResult Index(string name, string language)
+    {
+        string greetingMessage = CreateMessage(name, language);
+        string html = $"<h1 style='color: blue; font-family: Arial, sans-serif;'>{greetingMessage}</h1>";
+        return Content(html, "text/html");
+    }
 
    //Get: /helloworld/welcome
-    // POST: /helloworld/welcome
     [HttpGet("welcome/{name?}")]
-    [HttpPost("welcome")]
+
     public IActionResult Welcome(string name = "World")
     {
         return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
     }
+
+
+    public static string CreateMessage(string name, string language)
+     {
+         string helloTranslation = "";
+         switch (language)
+         {
+             case "french":
+                 helloTranslation = "Bonjour ";
+                 break;
+             case "spanish":
+                 helloTranslation = "Hola ";
+                 break;
+             case "bosnian":
+                 helloTranslation = "Zdravo ";
+                 break;
+             case "mongolian":
+                 helloTranslation = "Sain baina uu ";
+                 break;
+             case "english":
+                 helloTranslation = "Hello ";
+                 break;
+         }
+         return helloTranslation + name;
+
+     }
  
 
 }
